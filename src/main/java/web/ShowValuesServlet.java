d31855e1f8c597143f101456bd7c84c2b6cd94c5
tomaszociepa <tom.ociepa@gmail.com>
2018-12-09 21:06:49 +0100

    modify POST method

--- src/main/java/web/ShowValuesServlet.java
+++ src/main/java/web/ShowValuesServlet.java
@@ -4,6 +4,7 @@ import freemarker.TemplateProvider;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import model.Student;
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 import repository.StudentRepository;
 
 import javax.inject.Inject;
@@ -15,9 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @WebServlet(urlPatterns = "/infoShareAcademy")
 public class ShowValuesServlet extends HttpServlet {
@@ -56,15 +55,16 @@ public class ShowValuesServlet extends HttpServlet {
         resp.addHeader("Content-Type", "text/html; charset=utf-8");
         PrintWriter writer = resp.getWriter();
 
-        String param1 = req.getParameter("param1");
-        String param2 = req.getParameter("param2");
-        String[] param3 = req.getParameterValues("param3");
+        Map<String, String[]> parameterMap  = req.getParameterMap();
 
-        writer.println("param1: "+param1+"<br>");
-        writer.println("param2: "+param2+"<br>");
-        writer.println("param3: "+param3[0]+"<br>");
-        writer.println("param3: "+param3[1]);
+        parameterMap.forEach((k,v)->{
+           for (int i =0; i < v.length; i++){
+               writer.println(k +": "+ v[i]);
+               writer.println("<br>");
+           }
 
+       });
 
     }
+
 }
